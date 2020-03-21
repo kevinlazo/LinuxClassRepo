30a5f8be8dd7adfae40f410008f57fcad973ba90
kevinlazo <60993567+kevinlazo@users.noreply.github.com>
2020-03-20 23:18:58 -0400

    Update examQuestions.sh

--- Exams/Midterm/KevinLazo/examQuestions.sh
+++ Exams/Midterm/KevinLazo/examQuestions.sh
@@ -1,13 +1,84 @@
 question1(){
-	echo "todo"
+	echo "which command makes a lists things in a directory?"
+	echo "a) grep"
+        echo "b) cat"
+        echo "c) ls"
+        echo "d) bone"
+	read answer
+	case $answer in
+                "a")
+                        echo " wrong" >> $ANSWER_FILE
+                        ;;
+                "b")
+                        echo " wrong" >> $ANSWER_FILE
+                        ;;
+                "c")
+                        echo " right" >> $ANSWER_FILE
+                        ;;
+                "d")
+                        echo " wrong" >> $ANSWER_FILE
+                        ;;
+                *)
+                        echo "Please enter a valid selection from a-d"
+                        question1
+                        ;;
+        esac
 }
 
 question2(){
-	echo "todo"
+	echo "which command list hidden files"
+	echo "a) cat hidden files"
+        echo "b) sql xxx2"
+        echo "c) ls -a"
+        echo "d) grep hidden file finder"
+	read answer
+	case $answer in
+                "a")
+                        echo " wrong" >> $ANSWER_FILE
+                        ;;
+                "b")
+                        echo " wrong" >> $ANSWER_FILE
+                        ;;
+                "c")
+                        echo " right" >> $ANSWER_FILE
+                        ;;
+                "d")
+                        echo " wrong" >> $ANSWER_FILE
+                        ;;
+                *)
+                        echo "Please enter a valid selection from a-d"
+                        question1
+                        ;;
+        esac
+	
 }
 
 question3(){
-	echo "todo"
+	echo "which command lists the subdirectories"
+	echo "a) cat "
+        echo "b) subdirect me please "
+        echo "c) ls * "
+        echo "d) grep "
+	read answer
+	case $answer in
+                "a")
+                        echo " wrong" >> $ANSWER_FILE
+                        ;;
+                "b")
+                        echo " wrong" >> $ANSWER_FILE
+                        ;;
+                "c")
+                        echo " right" >> $ANSWER_FILE
+                        ;;
+                "d")
+                        echo " wrong" >> $ANSWER_FILE
+                        ;;
+                *)
+                        echo "Please enter a valid selection from a-d"
+                        question1
+                        ;;
+        esac
+	
 }
 
 
